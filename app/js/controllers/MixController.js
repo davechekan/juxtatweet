7a5eda596265ff3d2c1499cda4e4785f79b69fca
Dave Chekan <dave@chekan.com>
2013-08-01 02:15:28 -0400

    update styling on mix page

--- app/js/controllers/MixController.js
+++ app/js/controllers/MixController.js
@@ -2,6 +2,12 @@
 
 juxtatweet.controller('MixCtrl',
   function MixCtrl($scope) {
-    $scope.text = "This is the text I like";
-    $scope.imageUrl = "";
-  });
+    $scope.text = "\“Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.\”";
+    $scope.imageUrl = "http://farm6.staticflickr.com/5531/9402039830_db86f0c048_z.jpg";
+    $scope.author = "twittername";
+    $scope.authorUrl = "http://twitter.com/twittername";
+    $scope.imageHost = "flickr";
+    $scope.imageHostUrl = "http://www.flickr.com/photos/martinturner/9402039830";
+
+ });
+
